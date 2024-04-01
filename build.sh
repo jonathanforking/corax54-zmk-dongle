b58528f45e13b1acd174a8ac66a6ba00b7cee9db
jonathanforking <jonathanforking@proton.me>
2024-04-02 00:30:51 +0200

    Fixed build script config location flag

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 PWD=$(pwd)
 TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M")}"
 
-west build -s zmk/app -p -b nice_nano_v2 -- -DSHIELD="corax_left" -DMZK_CONFIG="${PWD}/config"
-mv build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_left.uf2"
+west build -s zmk/app -p -b nice_nano_v2 -- -DSHIELD="corax_left" -DZMK_CONFIG="${PWD}/config"
+cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_left.uf2"
 
-west build -s zmk/app -p -b nice_nano_v2 -- -DSHIELD="corax_right" -DMZK_CONFIG="${PWD}/config"
-mv build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_right.uf2"
+west build -s zmk/app -p -b nice_nano_v2 -- -DSHIELD="corax_right" -DZMK_CONFIG="${PWD}/config"
+cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_right.uf2"
 
-west build -s zmk/app -p -b seeeduino_xiao_ble -- -DSHIELD="corax_dongle" -DMZK_CONFIG="${PWD}/config"
-mv build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_dongle.uf2"
+west build -s zmk/app -p -b seeeduino_xiao_ble -- -DSHIELD="corax_dongle" -DZMK_CONFIG="${PWD}/config"
+cp build/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}_dongle.uf2"
